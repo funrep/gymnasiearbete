4847d875fe696c67b39033f6cf724df6408210f1
funrep <fuuunrep@gmail.com>
2015-11-09 21:06:46 +0100

    still errors

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
-import Tmx as T
+import tmx as T
 
-# Denna modulen innehåller funktionerna för att omvandla XML:en till ett Tmx objekt
+# Denna modulen innehåller funktionerna för att omvandla XML:en till ett tmx.Map objekt
 
 def parse_map(filename):
     xml = ET.parse(filename).getroot()
@@ -9,7 +9,7 @@ def parse_map(filename):
 
     version = get_item(xml_items, "version")
     width = get_item(xml_items, "width")
-    height = get_item(xml_items, height)
+    height = get_item(xml_items, "height")
     tilewidth = get_item(xml_items, "tilewidth")
     tileheight = get_item(xml_items, "tileheight")
 
@@ -20,23 +20,23 @@ def parse_map(filename):
         if child.tag == "tileset":
             tilesets.append(parse_tileset(child))
         elif child.tag == "layer":
-            layers.append(parse_layer(child)
+            layers.append(parse_layer(child))
 
-    tmx = T.Map()
+    return T.Map(version, width, height, tilewidth, tileheight, tilesets, layers)
 
 def parse_tileset(xml):
     xml_items = xml.items()
     firstgid = get_item(xml_items, "firstgid")
     name = get_item(xml_items, "name")
     image = parse_image(xml.getchildren()[0])
-    return Tileset(firstgid, name, image)
+    return T.Tileset(firstgid, name, image)
 
 def parse_image(xml):
     xml_items = xml.items()
     source = get_item(xml_items, "source")
     width = get_item(xml_items, "width")
-    height = get_height(xml_items "height")
-    return Image(source, width, height)
+    height = get_item(xml_items, "height")
+    return T.Image(source, width, height)
 
 def parse_layer(xml):
     name = get_item(xml.items(), "name")
@@ -45,7 +45,7 @@ def parse_layer(xml):
     for child in data:
         gid = get_item(child.items(), "gid")
         tiles.append(gid)
-    return Layer(name, tiles)
+    return T.Layer(name, tiles)
 
 def get_item(list_of_tuples, key):
     return [item for item in list_of_tuples if item[0] == key]
