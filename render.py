ecbcdff85663eb1b36ed4209701299cc81d8eba4
funrep <fuuunrep@gmail.com>
2015-11-11 19:20:12 +0100

    bit closer

--- render.py
+++ render.py
@@ -15,27 +15,54 @@ white = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
 
-tiles = pygame.image.load(gamemap.tilesets[0].image.source)
+image = gamemap.tilesets[0].image
+tileset = pygame.image.load(image.source)
+
+tiles = []
+for tile in gamemap.layers[0].tiles:
+    tiles.append(int(tile))
+
 
 def calc_offset(gid, img):
     columns = int(img.width) / 16
     rows = int(img.height) / 16
     tile_count = columns * rows
-    tileX, tileY = 1, 1 
-    i = 1
-    while tileX <= columns:
+    tileX, tileY = 0, 0
+    i = 0
+    while tileX < columns:
+        # print "Gid %d Iter %d x: %d y: %d" % (gid, i, tileX, tileY)
         if i == gid:
             return (16 * tileX, 16 * tileY, 16, 16)
-        elif tileX == columns:
-            tileX = 1
+        elif tileX == (columns - 1):
+            tileX = 0
             tileY += 1
         else:
             tileX += 1
         i += 1
 
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: sys.exit()
+# surfaces = []
+# for tile in tiles:
+#     offset = calc_offset(tile, image)
+#     print str(offset)
+#     surfaces.append(tileset.subsurface(offset))
 
-#     screen.fill(black)
-#     pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(black)
+    posX, posY = 0, 0
+    tileW, tileH = gamemap.tilewidth, gamemap.tileheight
+    i = 0
+    for tile in tiles:
+        offset = calc_offset(tile, image)
+        screen.blit(tileset, (posX, posY), offset)
+        if i == tileW:
+            posX = 0
+            posY += 16
+            i = 0
+        else:
+            posX += 16
+            i += 1
+
+    pygame.display.flip()
